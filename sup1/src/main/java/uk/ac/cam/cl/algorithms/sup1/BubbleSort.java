a729cdd46b98f126aa9d482b2572d4b2ff873a59
Lex van der Stoep <lex.vanderstoep@gmail.com>
2017-02-08 16:37:52 +0000

    Finished the coding for supervision 1

--- sup1/src/main/java/uk/ac/cam/cl/algorithms/sup1/BubbleSort.java
+++ sup1/src/main/java/uk/ac/cam/cl/algorithms/sup1/BubbleSort.java
@@ -2,15 +2,23 @@ package uk.ac.cam.cl.algorithms.sup1;
 
 import java.util.List;
 
-/**
- * Created by oliverchick on 24/12/2015.
- */
 public class BubbleSort<T extends Comparable> implements SortingAlgorithm<T> {
 
-    /*
-     * Implement bubble sort here
-     */
     public List<T> sort(List<T> input) {
-        return null;
+        int length = input.size();
+
+        // A maximum of 'length' sweeps through the list are required to sort it using bubble sort
+        for (int i = 0; i < length; i++) {
+            for (int j = 1; j < length - i; j++) {
+                // Test if two elements are out of order and if so, then swap them
+                if (input.get(j).compareTo(input.get(j - 1)) < 0) {
+                    T temp = input.get(j);
+                    input.set(j, input.get(j - 1));
+                    input.set(j - 1, temp);
+                }
+            }
+        }
+
+        return input;
     }
 }
